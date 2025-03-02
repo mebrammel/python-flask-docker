ea067a803dd0971b9f9160bdc3047412efdc6c3a
mebrammel <72341695+mebrammel@users.noreply.github.com>
2025-03-02 17:26:36 +0100

    fist commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route('/spy')
+def get_spy_price():
+    ticker = yf.Ticker("SPY")
+    hist = ticker.history(period="1d")  # Haal data voor de laatste dag op
+    price = hist['Close'].iloc[-1]  # Haal het sluitend prijspunt
+
+    return jsonify({
+        'symbol': "SPY",
+        'price': float(price)
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
